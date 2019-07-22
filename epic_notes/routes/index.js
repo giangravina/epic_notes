48436ca0fc7e98e12cfadafbd34e31182c694d2e
marisa <marisajurczyk@gmail.com>
2019-07-22 13:58:00 -0500

    fixed notes router

--- epic_notes/routes/index.js
+++ epic_notes/routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/notes', function(req, res, next){
-	res.render('notes');
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
