a53914340ef015a6e87e8adee35c8e42d3d317f6
Gian <giangravina@gmail.com>
2019-07-22 21:56:55 -0500

    updated home page

--- epic_notes/routes/index.js
+++ epic_notes/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Epic Notes', header: 'Welcome to our page!' });
 });
 
 module.exports = router;
