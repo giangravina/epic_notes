76f15e270ba6d9c1e90d696e4cc7566d7dc92c4e
Dominique <genjineedsmorehealing@gmail.com>
2019-07-23 21:00:03 -0500

    yet another attempt adding get and post

--- epic_notes/routes/notes.js
+++ epic_notes/routes/notes.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+// backend: listen for command request
+//    router.get('/route', callback( ) )
+//    router.post('url', callback( ) ) 
 router.get('/', function(req, res, next){
 	res.render('notes', {title: 'EpicNotes'});
 });
 
+router.get('/notes', function(req, res, next) {
+	// var n = [
+	// 	{title: "Music Dreams", note: "I love the stars"},
+	// 	{title: "To-Do List", note: "I must place the cat on the log"},
+	// 	{title: "Homework", note: "Math is lame, but trig must be done..."}
+	// 	]
+	var data = {note: $.get("#exampleFormControlTextarea1").val()};
+	$("#exampleFormControlTextarea1").val();
+	res.render('/notes');
+});
 
+router.post('/notes', function (req, res, next) {
+
+      $("#exampleFormControlTextarea1").val();
+	res.render('/notes');
+});
     
    
 
